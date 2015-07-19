1452e6132842bfb7e5f1b5d95db2c757215083fa
SamPaine <middletonsp@lopers.unk.edu>
2015-07-18 19:05:25 -0500

    This is the Game class... for games... dawg

---
+++ app/src/main/java/com/example/werehereforfood/appallnight/Game.java
@@ -0,0 +1,31 @@
+package com.example.werehereforfood.appallnight;
+
+/**
+ * Created by Sam on 7/18/2015.
+ */
+public class Game {
+
+    private String name;
+    private boolean complete;
+
+    public Game(String name, boolean complete) {
+        this.name = name;
+        this.complete = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+}
